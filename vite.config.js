31618ed510bd82938bee6e529f3b43056b6000c7
unknown <vmappsofficial@gmail.com>
2026-01-31 18:59:43 +0530

    we add more design

--- vite.config.js
+++ vite.config.js
@@ -3,11 +3,12 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "/fruit_project/",
   plugins: [
     react({
       babel: {
         plugins: [['babel-plugin-react-compiler']],
-        base: "/fruit_project/",
+        
       },
     }),
   ],
